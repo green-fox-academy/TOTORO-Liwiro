7a10f752b9de065345f305a2d5894a31f1c90fc8
tamasak <tamas.akos@gmail.com>
2017-08-31 14:00:59 +0200

    clean - not work

--- Projects/Liwiro_F7_client/Src/main.c
+++ Projects/Liwiro_F7_client/Src/main.c
@@ -1,53 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    LwIP/LwIP_HTTP_Server_Netconn_RTOS/Src/main.c 
-  * @author  MCD Application Team
-  * @version V1.2.0
-  * @date    30-December-2016
-  * @brief   This sample code implements a http server application based on 
-  *          Netconn API of LwIP stack and FreeRTOS. This application uses 
-  *          STM32F7xx the ETH HAL API to transmit and receive data. 
-  *          The communication is done with a web browser of a remote PC.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics International N.V. 
-  * All rights reserved.</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
-  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
 #include "ethernetif.h"
@@ -61,18 +11,12 @@
 #include "gui_setup.h"
 #include "WindowDLG.h"
 #include "k_bsp.h"
+#include "GUI.h"
+#include "WM.h"
 
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-//#define LCD_USERLOG			/*LCD userlog needed for IP address check */
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 struct netif gnetif; /* network interface structure */
 osTimerId lcd_timer;
 
-
-/* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void StartThread(void const * argument);
 static void BSP_Config(void);
@@ -83,77 +27,43 @@ static void CPU_CACHE_Enable(void);
 static void GUIThread(void const * argument);
 static void TimerCallback(void const *n);
 
-
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  * */
-
 int main(void)
 {
-	/* Configure the MPU attributes as Device memory for ETH DMA descriptors */
 	MPU_Config();
-
-	/* Enable the CPU Cache */
 	CPU_CACHE_Enable();
-
-	/* STM32F7xx HAL library initialization:
-	   - Configure the Flash ART accelerator on ITCM interface
-	   - Configure the Systick to generate an interrupt each 1 msec
-	   - Set NVIC Group Priority to 4
-	   - Global MSP (MCU Support Package) initialization
-	*/
 	HAL_Init();
-  
-	/* Configure the system clock to 200 MHz */
 	SystemClock_Config();
-
-	/* Initialize LCD */
 	BSP_Config();
 
-#ifndef LCD_USERLOG
-	 /* Initialize GUI */
+//#ifndef LCD_USERLOG
 	GUI_Init();
-
 	/* Activate the use of memory device feature */
 	WM_SetCreateFlags(WM_CF_MEMDEV);
 	WM_MULTIBUF_Enable(1);
 	GUI_SetLayerVisEx (1, 0);
 	GUI_SelectLayer(0);
 
-#endif
+//#endif
 	/* Create Touch screen Timer */
 	osTimerDef(TS_Timer, TimerCallback);
 	lcd_timer =  osTimerCreate(osTimer(TS_Timer), osTimerPeriodic, (void *)0);
-
 	/* Start the TS Timer */
 	osTimerStart(lcd_timer, 100);
-
 	/*Init thread */
 	osThreadDef(Start, StartThread, osPriorityHigh, 0, configMINIMAL_STACK_SIZE * 1);
 	osThreadCreate (osThread(Start), NULL);
-
-	/* Start scheduler */
 	osKernelStart();
-
-	/* We should never get here as control is now taken by the scheduler */
 	while(1) {
 	}
 }
-
 
 static void GUIThread(void const * argument)
 {
 
   MainTask();
-
-  /* Gui background Task */
   while(1) {
     GUI_Exec(); /* Do the background work ... Update windows etc.) */
-    osDelay(100); /* Nothing left to do for the moment ... Idle processing */
+    osDelay(20); /* Nothing left to do for the moment ... Idle processing */
   }
 }
 
@@ -172,28 +82,24 @@ osThreadId id;
   */
 static void StartThread(void const * argument)
 { 
-	/* Create tcp_ip stack thread */
-	tcpip_init(NULL, NULL);
+//	/* Create tcp_ip stack thread */
+//	tcpip_init(NULL, NULL);
+//
+//	/* Initialize the LwIP stack */
+//	Netif_Config();
+//
+//	/* Notify user about the network interface config */
+//	User_notification(&gnetif);
+//
+//	/* Start DHCPClient */
+//	osThreadDef(DHCP, DHCP_thread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2);
+//	osThreadCreate (osThread(DHCP), &gnetif);
 
-	/* Initialize the LwIP stack */
-	Netif_Config();
-
-	/* Notify user about the network interface config */
-	User_notification(&gnetif);
-
-	/* Start DHCPClient */
-	osThreadDef(DHCP, DHCP_thread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2);
-	osThreadCreate (osThread(DHCP), &gnetif);
-	//osDelay(2000);
-
-	/* Start httpserver thread */
-//	http_server_netconn_init();
-
-#ifndef LCD_USERLOG
+//#ifndef LCD_USERLOG
 	/* Create GUI task */
 	osThreadDef(GUI_Thread, GUIThread,   osPriorityBelowNormal, 0, configMINIMAL_STACK_SIZE * 2);
 	osThreadCreate (osThread(GUI_Thread), NULL);
-#endif
+//#endif
 
 	while (1) {
 		/* Delete the Init Thread */
@@ -240,20 +146,11 @@ static void Netif_Config(void)
   */
 static void BSP_Config(void)
 {
-
 	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
 	BSP_LED_Init(LED1);
-	/* Initialize the SDRAM */
 	BSP_SDRAM_Init();
-
-	/* Initialize the Touch screen */
-	//BSP_TS_Init(420, 272);
 	BSP_TS_Init(480, 272);
-
-	/* Enable CRC to Unlock GUI */
 	__HAL_RCC_CRC_CLK_ENABLE();
-
-	/* Enable Back up SRAM */
 	__HAL_RCC_BKPSRAM_CLK_ENABLE();
 
 #ifdef LCD_USERLOG
