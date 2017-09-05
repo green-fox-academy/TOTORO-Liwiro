9f6242ddec23b12b1f913af4fa5e85ecea0ce660
tamasak <tamas.akos@gmail.com>
2017-09-05 16:28:12 +0200

    added drawing area what sends coordinates - not working

--- Projects/Liwiro_PC_GUI/liwiro_pc_gui.c
+++ Projects/Liwiro_PC_GUI/liwiro_pc_gui.c
@@ -10,7 +10,7 @@
 #include <arpa/inet.h>
 #include <termios.h>
 
-#define SERVER_IP           "10.27.99.89"
+#define SERVER_IP           "10.27.99.111"
 #define SERVER_PORT         13003
 #define DATA_BUFFER_SIZE    1024
 
@@ -21,6 +21,26 @@ char getch(void);
 char getch_(int echo);
 char getch_(int echo);
 void initTermios(int echo);
+/* Surface to store current scribbles */
+static cairo_surface_t *surface = NULL;
+
+int32_t sqrt_measure(int32_t x)
+{
+	int32_t op, res, one;
+	op = x;
+	res = 0;
+	one = 1 << 30;
+	while (one > op) one >>= 2;
+	while (one != 0) {
+		if (op >= res + one) {
+			op = op - (res + one);
+			res = res +  2 * one;
+		}
+		res /= 2;
+		one /= 4;
+	}
+	return(res);
+}
 
 static void information_win (GtkWidget *wid, GtkWidget *win)
 {
@@ -56,6 +76,23 @@ void connect_to_server(int *client_sock, unsigned int server_port, char *server_
 		handle_error("connect() ");
 
 	printf("Connected to %s:%d\n", SERVER_IP, SERVER_PORT);
+}
+
+int send_message(uint8_t x, uint8_t y, uint8_t length)
+{
+	// Get the message from the user
+	uint8_t msg[4];
+	printf("Enter the message to send: \n");
+        msg[0] = 1;
+		msg[1] = x;
+		msg[2] = y;
+		msg[3] = length;
+	// Send the message to the servers
+	int sent_bytes = send(client_socket, msg, 4, 0);
+	if (sent_bytes < 0)
+		handle_error("send() ");
+
+	return sent_bytes;
 }
 
 int send_message_up(void)
@@ -143,6 +180,100 @@ int send_message_stop(void)
 	return sent_bytes;
 }
 
+static void clear_surface (void)
+{
+	cairo_t *cr;
+	cr = cairo_create (surface);
+	cairo_set_source_rgb (cr, 1, 1, 1);
+	cairo_paint (cr);
+	cairo_destroy (cr);
+}
+
+/* Create a new surface of the appropriate size to store our scribbles */
+static gboolean configure_event_cb (GtkWidget *widget, GdkEventConfigure *event, gpointer data)
+{
+	if (surface)
+		cairo_surface_destroy (surface);	
+	surface = gdk_window_create_similar_surface (gtk_widget_get_window (widget),
+                                               CAIRO_CONTENT_COLOR,
+                                               gtk_widget_get_allocated_width (widget),
+                                               gtk_widget_get_allocated_height (widget));
+	/* Initialize the surface to white */
+	clear_surface ();
+	/* We've handled the configure event, no need for further processing. */
+	return TRUE;
+}
+
+/* Redraw the screen from the surface. Note that the ::draw
+ * signal receives a ready-to-be-used cairo_t that is already
+ * clipped to only draw the exposed areas of the widget
+ */
+static gboolean draw_cb (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+	cairo_set_source_surface (cr, surface, 0, 0);
+	cairo_paint (cr);
+	return FALSE;
+}
+
+/* Draw a rectangle on the surface at the given position */
+static void draw_brush (GtkWidget *widget, gdouble x, gdouble y)
+{
+	cairo_t *cr;
+	/* Paint to the surface, where we store our state */
+	cr = cairo_create (surface);
+	cairo_rectangle (cr, x - 3, y - 3, 6, 6);
+	cairo_fill (cr);
+	cairo_destroy (cr);
+	/* Now invalidate the affected region of the drawing area. */
+	gtk_widget_queue_draw_area (widget, x - 3, y - 3, 6, 6);
+	printf("x=%lf\ty=%lf\n", x, y);
+	int32_t length2 = (int32_t)x*(int32_t)x + (int32_t)y*(int32_t)y;
+	int32_t length = sqrt_measure(length2);
+	if(length <= 100)
+		send_message((uint8_t)x, (uint8_t)y, (uint8_t)length);
+}
+
+/* Handle button press events by either drawing a rectangle
+ * or clearing the surface, depending on which button was pressed.
+ * The ::button-press signal handler receives a GdkEventButton
+ * struct which contains this information.
+ */
+static gboolean button_press_event_cb (GtkWidget *widget, GdkEventButton *event, gpointer data)
+{
+	/* paranoia check, in case we haven't gotten a configure event */
+	if (surface == NULL)
+		return FALSE;
+	if (event->button == GDK_BUTTON_PRIMARY) {
+		draw_brush (widget, event->x, event->y);
+	} else if (event->button == GDK_BUTTON_SECONDARY) {
+		clear_surface ();
+		gtk_widget_queue_draw (widget);
+    }
+	/* We've handled the event, stop processing */
+	return TRUE;
+}
+
+/* Handle motion events by continuing to draw if button 1 is
+ * still held down. The ::motion-notify signal handler receives
+ * a GdkEventMotion struct which contains this information.
+ */
+static gboolean motion_notify_event_cb (GtkWidget *widget, GdkEventMotion *event, gpointer data)
+{
+	/* paranoia check, in case we haven't gotten a configure event */
+	if (surface == NULL)
+		return FALSE;
+	if (event->state & GDK_BUTTON1_MASK)
+		draw_brush (widget, event->x, event->y);
+	/* We've handled it, stop processing */
+	return TRUE;
+}
+
+static void close_window (void)
+{
+	if (surface)
+		cairo_surface_destroy (surface);
+}
+
 int main (int   argc, char *argv[])
 {
 	int sent_bytes;
@@ -154,6 +285,7 @@ int main (int   argc, char *argv[])
 	GObject *window;
 	GObject *button;
 	GObject *event_box;
+	GObject *drawing_area;
 
 	gtk_init (&argc, &argv);
 
@@ -184,6 +316,13 @@ int main (int   argc, char *argv[])
 	g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
 	button = gtk_builder_get_object (builder, "information");
 	g_signal_connect (button, "clicked", G_CALLBACK (information_win), NULL);
+	
+	drawing_area = gtk_builder_get_object (builder, "drawingarea1");
+	g_signal_connect (drawing_area, "draw", G_CALLBACK (draw_cb), NULL);
+	g_signal_connect (drawing_area,"configure-event", G_CALLBACK (configure_event_cb), NULL);
+	/* Event signals */
+	g_signal_connect (drawing_area, "motion-notify-event", G_CALLBACK (motion_notify_event_cb), NULL);
+	g_signal_connect (drawing_area, "button-press-event", G_CALLBACK (button_press_event_cb), NULL);
 
 	gtk_main ();    
 
